47ac6c99a03959d17d86b69cc37319c86784d735
Dmitri <datitov2009@yandex.ru>
2023-11-26 15:12:32 +0300

    Change default window

--- TestProject1/src/main/java/com/example/testproject1/HelloController.java
+++ TestProject1/src/main/java/com/example/testproject1/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.testproject1;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    void initialize() {
+        btnSignIn.setOnAction(event -> {
+            System.out.println("Вы нажали кнопку Войти");
+        });
+
     }
+
 }
